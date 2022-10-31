d043e5101e6986b3ada189a4d6e46dfbe41e46db
vinicius <viniciusamalia@gmail.com>
2022-10-31 18:00:14 -0300

    correção das credenciais de email

--- src/main/java/br/com/lab/email/EmailConfig.java
+++ src/main/java/br/com/lab/email/EmailConfig.java
@@ -15,8 +15,8 @@ public class EmailConfig {
 	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 	    mailSender.setHost("smtp.gmail.com");
 	    mailSender.setPort(587);	    
-	    mailSender.setUsername("viniciusamalia@gmail.com");
-	    mailSender.setPassword("manocpbwigbecuwt");	    
+	    mailSender.setUsername("");
+	    mailSender.setPassword("");	    
 	    Properties props = mailSender.getJavaMailProperties();
 	    props.put("mail.transport.protocol", "smtp");
 	    props.put("mail.smtp.auth", "true");
